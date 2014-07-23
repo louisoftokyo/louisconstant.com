8e7b616f6c9a44b1fd15aed1c141e47d4940fd64
marunouchi <louis089@gmail.com>
2014-07-23 18:27:10 +0100

    Test commit

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Photos::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => redirect("en/")
+  root :to => redirect("en")
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
